2ef550f1a58338a372c02301cb42251c85c16e62
LeonidMorozov <lmmoby98@yandex.ru>
2016-08-28 14:38:10 +0300

    added search

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -4,13 +4,36 @@ class RentalsController < ApplicationController
 
   # GET /rentals.html
   def index
+    @rental_types = [
+        {id: 'apartment', name: 'Apartment'},
+        {id: 'holiday-home', name: 'Holiday Home'},
+        {id: 'villa', name: 'Villa'}
+    ]
+    # TODO: find out how to get pagination details from API response
     page = params[:page].to_i
     page = 1 if page <= 0
-    @rentals = bookingsync_api.rentals(per_page: 10, page: page)
+
+    search = search_params
+    if search.present?
+      @rentals = bookingsync_api.rentals_search(search)
+    else
+      @rentals = bookingsync_api.rentals(per_page: 10, page: page)
+    end
+
   end
 
   # GET /rentals/1.html
   def show
     @rental = bookingsync_api.rental(params[:id])
   end
+
+  private
+
+    def search_params
+      search = {}
+      search[:city] = params[:city].strip if params[:city].to_s.strip.present?
+      search[:rental_type] = params[:rental_type].join(',') if params[:rental_type].present?
+      search
+    end
+
 end
