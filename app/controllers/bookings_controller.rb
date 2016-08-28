08182fa2c19e4671f56c04169825a77709676f8f
LeonidMorozov <lmmoby98@yandex.ru>
2016-08-28 17:13:57 +0300

    some fixes

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,12 +5,17 @@ class BookingsController < ApplicationController
   # GET /bookings.html
   def index
     # TODO: ideally should be modal/popup window with search instead of select tag on @rentals and @bookings
-    @rentals = bookingsync_api.rentals({fields: [:id,:name]})
-    @clients = bookingsync_api.clients({fields: [:id,:fullname]})
-    @booking_statuses = [
-        {id: 'booked', name: 'Booked'},
-        {id: 'unavailable', name: 'Unavailable'},
-        {id: 'tentative', name: 'Tentative'}
+    cache_ttl = 1.hour
+    @rentals = Rails.cache.fetch("account_#{current_account.id}/rentals", expires_in: cache_ttl) do
+      bookingsync_api.rentals({fields: [:id,:name], auto_paginate: true})
+    end
+    @clients = Rails.cache.fetch("account_#{current_account.id}/clients", expires_in: cache_ttl) do
+      bookingsync_api.clients({fields: [:id,:fullname], auto_paginate: true})
+    end
+      @booking_statuses = [
+        {id: "booked", name: "Booked"},
+        {id: "unavailable", name: "Unavailable"},
+        {id: "tentative", name: "Tentative"}
     ]
     # TODO: find out how to get pagination details from API response
     page = params[:page].to_i
@@ -18,6 +23,7 @@ class BookingsController < ApplicationController
 
     search = search_params
     if search.present?
+      #TODO: expected bookingsync_api.bookings_search there
       @bookings = bookingsync_api.bookings(search)
     else
       @bookings = bookingsync_api.bookings(per_page: 10, page: page)
@@ -37,7 +43,7 @@ class BookingsController < ApplicationController
       search[:rental_id] = params[:rental_id].to_i if params[:rental_id].present?
       search[:client_id] = params[:client_id].to_i if params[:client_id].present?
       search[:booking_status] = params[:booking_status].strip.to_a if params[:booking_status].present?
-      search[:include_canceled] = ['true', true].include?(params[:include_canceled]) if params[:include_canceled].present?
+      search[:include_canceled] = ["true", true].include?(params[:include_canceled]) if params[:include_canceled].present?
       search
     end
 
