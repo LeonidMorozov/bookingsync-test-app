007545deac6712020d07f10942c92a5f0b852f99
LeonidMorozov <lmmoby98@yandex.ru>
2016-08-30 14:09:22 +0300

    added comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,7 @@ class ApplicationController < ActionController::Base
       #TODO: handle token expired/revoked/etc
       # I think it can be something in `current_account` to make auth back to live
       # it has no re-auth or refresh token mechanism there, at least I didn't find it
+      #TODO: FIXME: @api should be in current_account context of course
       @api ||= BookingSync::API.new(current_account.oauth_access_token, logger: Rails.logger)
     end
 
